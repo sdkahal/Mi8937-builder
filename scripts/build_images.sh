4d3049a2522d80aef175370bfa798929320efb9a
Almond <88301593+sdkahal@users.noreply.github.com>
2026-05-06 15:40:56 +0800

    Refactor dtb file extraction in build_images.sh
    Simplified extraction of dtb file by removing intermediate directory.

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -11,8 +11,7 @@ truncate -s 66060288 boot.raw
 mkfs.ext2 boot.raw
 mount boot.raw mnt
 tar xf rootfs.tgz -C mnt ./boot --exclude='./boot/linux.efi' --exclude='./boot/dtbs/*' --strip-components=2
-mkdir -p mnt/dtbs/qcom
-tar xf rootfs.tgz -C mnt/dtbs/qcom --wildcards '*/msm8937-xiaomi-land.dtb' --strip-components=5
+tar xf rootfs.tgz -C mnt --wildcards '*/msm8937-xiaomi-land.dtb' --strip-components=2
 umount mnt
 
 # create root img
